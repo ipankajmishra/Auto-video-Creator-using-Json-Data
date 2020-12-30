f543e931557c8f5c3b7a8d70d7523feac2c5eeb9
Pankaj Mishra <pankaj.mishra2902@gmail.com>
2020-12-31 02:37:43 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,70 +1,136 @@
-import { Component } from 'react';
+import { Component } from "react";
 import { CanvasJSChart, CanvasJS } from "canvasjs-react-charts";
-import html2canvas from 'html2canvas';
+import html2canvas from "html2canvas";
+import { dataStore } from "./data.json";
 class App extends Component {
-  saveAsImage(){
-    var container = document.getElementById("chartContainer");; // full page 
-            html2canvas(container, { allowTaint: true }).then(function (canvas) {
-
-                var link = document.createElement("a");
-                document.body.appendChild(link);
-                link.download = "html_image.jpg";
-                link.href = canvas.toDataURL();
-                link.target = '_blank';
-                link.click();
-            });
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataPoints: [],
+      currentYear: "",
+      optionsArray: [],
+      optionsArray2: [],
+    };
   }
-  
-	render() {
-		const options = {
-			animationEnabled: true,
-			theme: "light2",
-			title:{
-				text: "Most Popular Social Networking Sites"
-			},
-			axisX: {
-				title: "Social Network",
-				reversed: true,
-			},
-			axisY: {
-				title: "Monthly Active Users",
-				includeZero: true,
-				labelFormatter: this.addSymbols
-      },
-      export: true,
-			data: [{
-				type: "bar",
-				dataPoints: [
-					{ y:  2200000000, label: "Facebook" },
-					{ y:  1800000000, label: "YouTube" },
-					{ y:  800000000, label: "Instagram" },
-					{ y:  563000000, label: "Qzone" },
-					{ y:  376000000, label: "Weibo" },
-					{ y:  336000000, label: "Twitter" },
-					{ y:  330000000, label: "Reddit" }
-				]
-			}]
+  showGraphs() {
+    this.setState({ optionsArray2: this.state.optionsArray });
+  }
+
+  saveAsImage(data) {
+    this.setState({ dataPoints: data }, () => {
+      var container = document.getElementById("chartContainer"); // full page
+      html2canvas(container, { allowTaint: true }).then(function (canvas) {
+        var link = document.createElement("a");
+        document.body.appendChild(link);
+        link.download = "html_image.jpg";
+        link.href = canvas.toDataURL();
+        link.target = "_blank";
+        link.click();
+        // this.wait(3000);
+        this.setState({
+          dataPoints: [],
+        });
+      });
+    });
+  }
+
+  componentDidMount() {
+    console.log(dataStore);
+
+    for (let i in dataStore) {
+      console.log(dataStore[i]);
+      let dataPoint = [];
+      let data = dataStore[i];
+      for (let key in data) {
+        if (data.hasOwnProperty(key)) {
+          let obj = {};
+          if (key !== "Date") {
+            obj["y"] = data[key];
+            obj["label"] = key;
+            dataPoint.push(obj);
+          }
+        }
+      }
+      let array = this.state.optionsArray;
+      array.push(dataPoint);
+      this.setState({ optionsArray: array });
+      //   this.setState({dataPoints: dataPoint},()=>{
+      //   this.saveAsImage(dataPoint);
+      //   this.wait(3000);
+      //   this.setState({dataPoints:[]},()=>{
+      // console.log(dataPoint);
+      // dataPoint = [];
+      // console.log(dataPoint);
+      //   });
+      // console.log(this.state.dataPoints, this.state.currentYear);
+      // console.log("before");
+      // // this.wait(5000);
+      // console.log("after");
+      //   });
     }
-		return (
+  }
+
+  wait(ms) {
+    var start = new Date().getTime();
+    var end = start;
+    while (end < start + ms) {
+      end = new Date().getTime();
+    }
+  }
+
+  render() {
+    
+    const mapData = this.state.optionsArray2.map((data, key) => {
+      let options = {
+        animationEnabled: false,
+        theme: "light2",
+        title: {
+          text: "Most Popular Web Browsers",
+        },
+        culture: "es",
+        axisX: {
+          title: "Web Browsers",
+          reversed: true,
+          // labelAngle: -70,
+          interval: 1,
+        },
+        axisY: {
+          title: "Monthly Active Users % in " + key,
+          includeZero: true,
+          interval: 10,
+          maximum: 100,
+          minimum: 0,
+        },
+        data: [
+          {
+            type: "bar",
+            dataPoints: data,
+          },
+        ],
+      };
+      return (
+        <div id="key">
+          <CanvasJSChart key={"graph" + key} options={options} />
+        </div>
+      );
+    });
+    return (
       <>
-		<div id="chartContainer">
-			<CanvasJSChart options = {options}
-			/>
-      
-		</div>
-    <div>
-    <button onClick={()=>this.saveAsImage()}>Save as</button>
-  </div>
-  </>
-		);
-	}
-	addSymbols(e){
-		var suffixes = ["", "K", "M", "B"];
-		var order = Math.max(Math.floor(Math.log(e.value) / Math.log(1000)), 0);
-		if(order > suffixes.length - 1)
-			order = suffixes.length - 1;
-		var suffix = suffixes[order];
-		return CanvasJS.formatNumber(e.value / Math.pow(1000, order)) + suffix;
-	}
+        {mapData}
+
+        <div>
+          <button onClick={() => this.showGraphs()}>Save as</button>
+        </div>
+      </>
+    );
+  }
+  // addSymbols(e){
+  // 	var suffixes = ["", "K", "M", "B"];
+  // 	var order = Math.max(Math.floor(Math.log(e.value) / Math.log(1000)), 0);
+  // 	if(order > suffixes.length - 1)
+  // 		order = suffixes.length - 1;
+  // 	var suffix = suffixes[order];
+  // 	return CanvasJS.formatNumber(e.value / Math.pow(1000, order)) + suffix;
+  // }
 }
-export default App; 
+export default App;
